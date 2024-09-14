ce00afbe77ca750af1f5f028bcb8a692be73e18f
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-14 21:25:39 +0500

    Add project for Day 2

--- 100-Days-of-Code/Day2/Tip Calculator Project/task.py
+++ 100-Days-of-Code/Day2/Tip Calculator Project/task.py
@@ -1,5 +1,5 @@
 # Task: Tip Calculator
 # TODO: Create a tip calculator that calculates how much each person should pay, including the tip.
 # Instructions: Write a program that asks for the total bill, tip percentage, and number of people, then calculates how much each person should pay. Round the final amount to 2 decimal places.
-# Example:
+# Example
 # Each person should pay: ${final_amount}
