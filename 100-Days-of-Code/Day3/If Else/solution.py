a27bb45f407ed78102e83741d06c472e2bdb85ee
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-16 13:03:44 +0500

    Add task and solution file for If Else

---
+++ 100-Days-of-Code/Day3/If Else/solution.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 100:
+    print("You can ride on rollercoaster")
+else :
+    print("Your height should be 100 cm or above 12 ")
+
---
+++ 100-Days-of-Code/Day3/If Else/task.py
@@ -0,0 +1,2 @@
+# Task : Rollercoaster Ride Eligibility using conditional statements
+# TODO: Determine if a person can ride the rollercoaster based on their height.
