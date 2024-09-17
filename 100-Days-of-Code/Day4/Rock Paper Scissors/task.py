a7a9d49852fbf201c16a3c1f3e41315c3a7060c2
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-17 22:09:35 +0500

    Add project for day 4

---
+++ 100-Days-of-Code/Day4/Rock Paper Scissors/task.py
@@ -0,0 +1,39 @@
+# Task: Rock, Paper, Scissors Game
+# TODO: Create ASCII art representations of rock, paper, and scissors and store them in a list. (already done)
+
+# TODO: Prompt the user to make a choice by typing 0 for Rock, 1 for Paper, or 2 for Scissors.
+
+# TODO: Validate the user's choice to ensure it's between 0 and 2, and print an error message if it's invalid.
+
+# TODO: Print the user's chosen image (Rock, Paper, or Scissors) based on their input.
+
+# TODO: Have the computer randomly select between Rock, Paper, and Scissors, and print the computer's choice.
+
+# TODO: Implement the game logic to determine the winner or if it's a draw, and print the result accordingly.
+
+rock = '''
+     _______
+ ---'   ____)
+       (_____)
+       (_____)
+       (____)
+ ---.__(___)
+ '''
+
+paper = '''
+     _______
+ ---'   ____)____
+           ______)
+           _______)
+          _______)
+ ---.__________)
+ '''
+
+scissors = '''
+     _______
+ ---'   ____)____
+           ______)
+        __________)
+       (____)
+ ---.__(___)
+ '''
