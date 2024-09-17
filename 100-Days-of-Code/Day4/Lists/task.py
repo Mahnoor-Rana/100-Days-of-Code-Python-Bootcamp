d6a04b472861b8253165f13eb15917b301f518db
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-17 21:14:25 +0500

    Add task and solution for List

---
+++ 100-Days-of-Code/Day4/Lists/task.py
@@ -0,0 +1,8 @@
+# Task: Modifying and Extending the List of States in the USA
+# TODO: Print the second state in the list of states
+
+# TODO: Modify the second state in the list to "Pakistan" and print the updated state
+
+# TODO: Append a new state "Angelaland" to the list of states
+
+# TODO: Extend the list by adding two new states: "Angelaland" and "Jack Bauer Land"
