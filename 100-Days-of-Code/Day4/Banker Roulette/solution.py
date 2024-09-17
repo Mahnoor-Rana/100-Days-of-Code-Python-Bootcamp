90f8f4214c902d838e5860f6813435d6f5ed7abb
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-17 21:26:38 +0500

    Add task and solution for Banker Roulette module

---
+++ 100-Days-of-Code/Day4/Banker Roulette/solution.py
@@ -0,0 +1,7 @@
+import random
+friends = ["Mahnoor", "Aleena", 'Hamza',"Aqsa","Usman","Khan"]
+print(random.choice(friends))
+
+# we can also do this by randint
+rand_index = random.randint(0,4)
+print(rand_index)
---
+++ 100-Days-of-Code/Day4/Banker Roulette/task.py
@@ -0,0 +1,4 @@
+# Task: Banker Roulette - Randomly Selecting a Friend
+# TODO: Print a randomly selected friend from the list using `random.choice`.
+
+# TODO: Generate a random index between 0 and 4 using `random.randint` and print the index.
