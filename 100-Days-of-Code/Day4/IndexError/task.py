f9183cd3014220b6c2a5d1537006151438de7ac6
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-17 21:41:53 +0500

    Add task and solution OF index Error and Nested list

---
+++ 100-Days-of-Code/Day4/IndexError/task.py
@@ -0,0 +1,7 @@
+# Task: Accessing and Manipulating Lists
+# Index Error and Nested List
+# TODO: Print the 50th state in the `states_of_america` list and ensure that the index is within range.
+# TODO: Print the number of items in the `states_of_america` list using `len()` and print the last item in the list.
+
+# Task: Creating and Printing Nested Lists
+# TODO: Create a nested list `dirty_dozen` containing two lists: one for fruits and one for vegetables, and print the nested list.
