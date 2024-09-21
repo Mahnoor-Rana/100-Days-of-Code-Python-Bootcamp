9c62a4955e10b653e11bcb78bca7bad8fa5d8441
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2024-09-21 21:40:55 +0500

    Add project for day 5

---
+++ 100-Days-of-Code/Day5/Password Generator Project/task.py
@@ -0,0 +1,6 @@
+# Task: Password Generator
+# TODO: Import the random module and create lists for letters, numbers, and symbols.
+#       Prompt the user for the number of letters, symbols, and numbers they want in their password.
+#       Generate a list of random choices from these lists based on user input.
+#       Shuffle the list to ensure randomness and then combine the elements to form the final password.
+#       Finally, print the generated password.
